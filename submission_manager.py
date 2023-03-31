ef0853fd2a0d4405fb91367ba658f5e2ed508fa0
Nikolaj Rønne <nronne@phys.au.dk>
2023-03-31 14:52:59 +0200

    chagnes

--- submission_manager.py
+++ submission_manager.py
@@ -1,72 +1,80 @@
-import subprocess
 import os
+import subprocess
+
 
 class MissingRunscript(Exception):
     pass
 
-class SubmissionManager():
 
-    def __init__(self, path, config, array, partition, mem, ntasks_per_node, time, gpus, arguments='',
-                 nodes=1, cpus_per_task=1, run_in_data_dir=True):
-
+class SubmissionManager:
+    def __init__(
+        self,
+        path,
+        config,
+        array,
+        partition,
+        mem,
+        ntasks_per_node,
+        time,
+        gpus,
+        exec_command,
+        venv,
+        venv_command,
+        arguments="",
+        nodes=1,
+        cpus_per_task=1,
+        run_in_data_dir=True,
+    ):
         self.run_in_data_dir = run_in_data_dir
-        
+
         self.set_paths(path)
         self.config = config
-        
+
         self.set_array(array)
-        
+
         self.partition = partition
         self.mem = mem
         self.ntasks_per_node = ntasks_per_node
         self.time = time
 
         self.arguments = arguments
-        
+
         self.nodes = nodes
         self.cpus_per_task = cpus_per_task
         self.gpus = gpus
 
-
-
+        self.exec_command = exec_command
+        self.venv = venv
+        self.venv_command = venv_command
 
     def set_array(self, array):
-        assert isinstance(array, list), 'Array must be list of strings'
         if len(array) == 1:
             try:
-                i = int(array[0])-1
-                self.array = f'0-{i}'
-                self.array_list = list(range(i))
+                i = int(array[0])
+                self.array = f"0-{i-1}"
             except:
                 self.array = array[0]
-                array_split = array[0].split('%')[0].split('-')
-                self.array_list = list(range(array_split[0], array_split[1]))
 
         else:
-            self.array = ''.join([i+',' for i in array])[:-1]
-            self.array_list = [int(i) for i in array]
+            self.array = "".join([i + "," for i in array])[:-1]
 
-
-            
-                
-        
     def set_paths(self, path):
         self.full_path = os.path.abspath(path)
 
         if not os.path.isfile(self.full_path):
-            raise MissingRunscript('No runscript file is found!')
-        
+            raise MissingRunscript("No runscript file is found!")
+
         self.directory_path = os.path.dirname(self.full_path)
         self.runscript_name = os.path.basename(self.full_path)
         self.runscript_name_no_ext, _ = os.path.splitext(self.runscript_name)
         self.main_dir, self.directory_name = os.path.split(self.directory_path)
 
-        if self.directory_name == 'runscripts':
-            self.data_dir = os.path.join(self.main_dir, 'data')
+        if self.directory_name == "runscripts":
+            self.data_dir = os.path.join(self.main_dir, "data")
             if not os.path.exists(self.data_dir):
                 os.makedirs(self.data_dir)
         else:
-            self.data_dir = os.path.join(self.directory_path + '/data')
+            self.data_dir = os.path.join(self.directory_path + "/data")
             if not os.path.exists(self.data_dir) and self.run_in_data_dir:
                 os.makedirs(self.data_dir)
 
@@ -74,30 +82,27 @@ class SubmissionManager():
             self.run_dir = os.path.join(self.data_dir, self.runscript_name_no_ext)
         else:
             self.run_dir = os.path.join(self.directory_path, self.runscript_name_no_ext)
-            
+
         if not os.path.exists(self.run_dir):
             os.makedirs(self.run_dir)
 
-
         self.paths = {
-            'full_path': self.full_path,
-            'directory_path': self.directory_path,
-            'runscript_name': self.runscript_name,
-            'runscript_name_no_ext': self.runscript_name_no_ext,
-            'main_dir': self.main_dir,
-            'directory_name': self.directory_path,
-            'data_dir': self.data_dir,
-            'run_dir': self.run_dir
+            "full_path": self.full_path,
+            "directory_path": self.directory_path,
+            "runscript_name": self.runscript_name,
+            "runscript_name_no_ext": self.runscript_name_no_ext,
+            "main_dir": self.main_dir,
+            "directory_name": self.directory_path,
+            "data_dir": self.data_dir,
+            "run_dir": self.run_dir,
         }
 
-
-
     def make_job_file(self, sub_type, scratch=True, dry=False):
-        lb = '\n'
-        C = self.config['DEFAULT']['COMMAND']
-        
-        s = self.config['DEFAULT']['SHEBANG']
-        s += lb        
+        lb = "\n"
+        C = self.config["DEFAULT"]["COMMAND"]
+
+        s = self.config["DEFAULT"]["SHEBANG"]
+        s += lb
         s += f"""{C} --job-name={self.runscript_name_no_ext}
 {C} --partition={self.partition}
 {C} --nodes={self.nodes}
@@ -105,47 +110,51 @@ class SubmissionManager():
 {C} --cpus-per-task={self.cpus_per_task}
 {C} --mem={self.mem}G
 {C} --time={self.time}:00:00"""
-        s += lb
-        
-        if sub_type == 'ARRAY':
-            s += f'{C} --array={self.array}'
 
         s += lb
-        if self.gpus !=0:
-            s += f'{C} --gres=gpu:{self.gpus}'
-        
-        s += lb
-        s += self.config['DEFAULT']['SUBMIT'].format(C)
+
+        if sub_type == "ARRAY":
+            s += f"{C} --array={self.array}"
 
         s += lb
-        s += self.config['DEFAULT']['LOAD']
+        if self.gpus != 0:
+            s += f"{C} --gres=gpu:{self.gpus}"
+
+        s += lb
+        s += self.config["DEFAULT"]["SUBMIT"].format(C)
+
+        s += lb
+        s += self.config["DEFAULT"]["LOAD"].format(
+            venv_command=self.venv_command, venv=self.venv
+        )
         s += lb
 
         s += lb
-        s += self.config[sub_type]['PRE'].format(**self.paths)
-        s += lb        
+        s += self.config[sub_type]["PRE"].format(**self.paths)
+        s += lb
 
         if scratch:
-            s += self.config[sub_type]['PRESCRATCH'].format(**self.paths)
+            s += self.config[sub_type]["PRESCRATCH"].format(**self.paths)
             s += lb
-        
-        s += self.config[sub_type]['DO'].format(**self.paths, arguments=self.arguments)
-        
+
+        s += self.config[sub_type]["DO"].format(
+            **self.paths, arguments=self.arguments, exec_command=self.exec_command
+        )
+
         if scratch:
-            s += self.config[sub_type]['POSTSCRATCH'].format(**self.paths)
+            s += self.config[sub_type]["POSTSCRATCH"].format(**self.paths)
             s += lb
-        
-        s += lb        
-        s += self.config[sub_type]['POST'].format(**self.paths)
+
+        s += lb
+        s += self.config[sub_type]["POST"].format(**self.paths)
 
         if dry:
             print(s)
         else:
-            with open(os.path.join(self.run_dir, 'run.job'), 'w') as f:
+            with open(os.path.join(self.run_dir, "run.job"), "w") as f:
                 f.write(s)
-
 
     def submit_job(self):
         os.chdir(self.run_dir)
-        sbatch_out = str(subprocess.check_output('sbatch run.job', shell=True))
+        sbatch_out = str(subprocess.check_output("sbatch run.job", shell=True))
         print(sbatch_out)
--- submit.py
+++ submit.py
@@ -1,103 +1,109 @@
 import argparse
+
 from read_config import read
 from submission_manager import SubmissionManager
 
+
 def array():
     parser = argparse.ArgumentParser()
-    parser.add_argument('runscript', type=str)
-    parser.add_argument('args', nargs='?', default='-i $SLURM_ARRAY_TASK_ID', type=str)
-    parser.add_argument('-i', '--array', nargs='+',
-                        help="The array job array to start. Either as int, slurm input str or several ints")
-    
-    parser.add_argument('-p', '--partition', type=str)
-    parser.add_argument('-m', '--mem', type=int)
-    parser.add_argument('-n', '--ntasks-per-node', dest='ntasks_per_node', type=int)
-    parser.add_argument('-t', '--time', type=int, help='Time in hours')
-    parser.add_argument('-g', '--gpus', type=int, help='Number of GPUS')
-    # parser.add_argument('-a', '--args', type=str, default='-i $SLURM_ARRAY_TASK_ID', help='arguments for runscript')
-    
-    parser.add_argument('--submit-on-main', dest='scratch', action='store_false')
-    parser.add_argument('--dry-run', dest='dry', action='store_true')
-    parser.add_argument('--config-file', dest='config_file', type=str)
+    parser.add_argument("runscript", type=str)
+    # parser.add_argument("args", nargs="?", default="-i $SLURM_ARRAY_TASK_ID", type=str)
+    parser.add_argument(
+        "-i",
+        "--array",
+        nargs="+",
+        help="The array job array to start. Either as int, slurm input str or several ints",
+    )
+
+    parser.add_argument("-p", "--partition", type=str)
+    parser.add_argument("-m", "--mem", type=int)
+    parser.add_argument("-n", "--ntasks-per-node", dest="ntasks_per_node", type=int)
+    parser.add_argument("-t", "--time", type=int, help="Time in hours")
+    parser.add_argument("-g", "--gpus", type=int, help="Number of GPUS")
+
+    parser.add_argument(
+        "-A",
+        "--args",
+        type=str,
+        default="-i $SLURM_ARRAY_TASK_ID",
+        help="Arguments for runscript. Default: -i $SLURM_ARRAY_TASK_ID. Add space before -",
+    )
+
+    parser.add_argument(
+        "-V", "--venv", type=str, help="Name or path to virtual environment"
+    )
+    parser.add_argument(
+        "--venv-command",
+        type=str,
+        dest="venv_command",
+        help="Command to activate virtual environment",
+    )
+
+    parser.add_argument(
+        "-E",
+        "--exec-command",
+        dest="exec_command",
+        type=str,
+        help="Program execution command",
+    )
+
+    parser.add_argument("-M", "--submit-on-main", dest="scratch", action="store_false")
+    parser.add_argument("-D", "--dry-run", dest="dry", action="store_true")
+    parser.add_argument("-C", "--config-file", dest="config_file", type=str)
+    parser.add_argument(
+        "--data-dir",
+        dest="data_dir",
+        action="store_false",
+        help="Whether to run in seperate data directory",
+    )
+
     args = parser.parse_args()
 
-    
     config = read(args.config_file)
-    
-    
+
     if args.array is None:
-        args.array = [config['DEFAULT']['array']]
+        args.array = [config["DEFAULT"]["array"]]
 
     if args.partition is None:
-        args.partition = config['DEFAULT']['partition']
+        args.partition = config["DEFAULT"]["partition"]
 
     if args.mem is None:
-        args.mem = config['DEFAULT']['mem']                
+        args.mem = config["DEFAULT"]["mem"]
 
     if args.ntasks_per_node is None:
-        args.ntasks_per_node = config['DEFAULT']['ntasks-per-node']
+        args.ntasks_per_node = config["DEFAULT"]["ntasks-per-node"]
 
     if args.time is None:
-        args.time = config['DEFAULT']['time']        
+        args.time = config["DEFAULT"]["time"]
 
     if args.gpus is None:
-        args.gpus = config['DEFAULT'].get('gpus', 0)
+        args.gpus = config["DEFAULT"].get("gpus", 0)
 
-    sm = SubmissionManager(args.runscript, config, args.array, args.partition, args.mem, args.ntasks_per_node,
-                           args.time, args.gpus, arguments=args.args)
-    
-    
-    sm.make_job_file('ARRAY', scratch=args.scratch, dry=args.dry)
+    if args.venv is None:
+        args.venv = config["DEFAULT"].get("venv", "")
+
+    if args.venv_command is None:
+        args.venv_command = config["DEFAULT"].get("venv_command", "")
+
+    if args.exec_command is None:
+        args.exec_command = config["DEFAULT"].get("exec_command", "")
+
+    sm = SubmissionManager(
+        path=args.runscript,
+        config=config,
+        array=args.array,
+        partition=args.partition,
+        mem=args.mem,
+        ntasks_per_node=args.ntasks_per_node,
+        time=args.time,
+        gpus=args.gpus,
+        exec_command=args.exec_command,
+        venv=args.venv,
+        venv_command=args.venv_command,
+        arguments=args.args,
+        run_in_data_dir=args.data_dir,
+    )
+
+    sm.make_job_file("ARRAY", scratch=args.scratch, dry=args.dry)
     if not args.dry:
         sm.submit_job()
-    
-
-    
-def sub():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('runscript', type=str)
-    parser.add_argument('args', nargs='?', default='-i $SLURM_ARRAY_TASK_ID', type=str)
-    
-    parser.add_argument('-p', '--partition', type=str)
-    parser.add_argument('-m', '--mem', type=int)
-    parser.add_argument('-n', '--ntasks-per-node', dest='ntasks_per_node', type=int)
-    parser.add_argument('-t', '--time', type=int, help='Time in hours')
-    parser.add_argument('-g', '--gpus', type=int, help='Number of GPUS')    
-    # parser.add_argument('-a', '--args', type=str, default='', help='arguments for runscript')
-    
-    parser.add_argument('--submit-on-scratch', dest='scratch', action='store_true')
-    parser.add_argument('--dry-run', dest='dry', action='store_true')
-    parser.add_argument('--no-data-dir', dest='run_in_data_dir', action='store_false')
-    parser.add_argument('--config-file', dest='config_file', type=str)
-    args = parser.parse_args()
-
-    
-    config = read(args.config_file)
-    
-    
-    if args.partition is None:
-        args.partition = config['DEFAULT']['partition']
-
-    if args.mem is None:
-        args.mem = config['DEFAULT']['mem']                
-
-    if args.ntasks_per_node is None:
-        args.ntasks_per_node = config['DEFAULT']['ntasks-per-node']
-
-    if args.time is None:
-        args.time = config['DEFAULT']['time']
-
-    if args.gpus is None:
-        args.gpus = config['DEFAULT'].get('gpus', 0)
-
-    
-
-    sm = SubmissionManager(args.runscript, config, ['0'], args.partition, args.mem, args.ntasks_per_node, args.time, args.gpus,
-                           arguments=args.args, nodes=config['DEFAULT']['nodes'],
-                           cpus_per_task=config['DEFAULT']['cpus-per-task'], run_in_data_dir=args.run_in_data_dir)
-    
-    
-    sm.make_job_file('SUB', scratch=args.scratch, dry=args.dry)
-    if not args.dry:
-        sm.submit_job()
-
